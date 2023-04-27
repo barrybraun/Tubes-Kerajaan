f6da6345111004cb921f1f18334fb9735ea29cfc
AqilaGhifariWandana <aqila.ghifari.tif22@polban.ac.id>
2023-04-27 17:03:35 +0700

    penambahan header

--- header.h
+++ header.h
@@ -1,1 +1,33 @@
 // buat header
+#ifndef header_H
+#define header_H
+
+#define MALE 1
+#define FEMALE 0
+
+#include <stdio.h>
+#include <stdbool.h>
+#include <time.h>
+
+typedef char nbType[20];
+typedef struct royalTree *nbAddr;
+
+struct identity{
+    nbType name;
+    struct tm birthDate; // tipe data struct tm menyimpan tanggal lahir
+    bool gender;
+};
+
+typedef struct royalTree{
+	nbAddr fs;
+	nbAddr nb;
+	nbAddr parent;
+	identity info;
+	identity partner;
+}nbTreeNode;
+
+struct nbTree{
+    nbAddr root;
+};
+
+#endif
